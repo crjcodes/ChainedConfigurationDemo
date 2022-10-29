1be14e2a9728372eea4ed22706bec515aa5c35ff
crjcodes <crjcodes@users.noreply.github.com>
2022-10-29 08:09:14 -0500

    Initial commit to repo

---
+++ ChainedConfigurationDemo/HostBuilderExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace ChainedConfigurationDemo;
+
+public static class HostBuilderExtensions
+{
+    public static HostBuilderContext LogConfiguration(this HostBuilderContext hostContext)
+    {
+        Console.WriteLine("Logging Starting Configuration Via Host Context");
+
+        // Add keywords here to exclude certain environmental variables, properties, and settings from logging
+        // Screening here, as opposed to launchSettings or other configuration mechanism,
+        // to avoid cluttering logging of the configuration
+
+        var toExclude = new List<string>
+        {
+            "_",
+            "APPDATA",
+            "COMMON",
+            "COMPAT_LAYER",
+            "COMPUTER",
+            "COMSPEC",
+            "CONFIG",
+            "content",
+            "DISPLAY",
+            "DRIVER",
+            "HOME",
+            "HOSTNAME",
+            "LOGON",
+            "MSBUILD",
+            "OneDrive",
+            "PATH",
+            "PS1",
+            "PROGRAM",
+            "PROCESSOR",
+            "PROCESSOR",
+            "PUBLIC",
+            "PWD",
+            "SSH_",
+            "SHL",
+            "SHELL",
+            "SYSTEM",
+            "TEMP",
+            "TERM",
+            "TMP",
+            "USER",
+            "WINDIR",
+            "MINGW",
+        };
+
+        var configurationPairs = hostContext.Configuration.AsEnumerable();
+
+        foreach (var pair in configurationPairs)
+        {
+            if (toExclude.Any(exc => pair.Key.Contains(exc, StringComparison.InvariantCultureIgnoreCase)))
+                continue;
+
+            Console.WriteLine("Key: {0}, Value: {1}", pair.Key, pair.Value);
+        }     
+
+        return hostContext;
+    }
+}
---
+++ ChainedConfigurationDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using ChainedConfigurationDemo;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics.Eventing.Reader;
+
+Console.WriteLine("Starting demo");
+Console.WriteLine("Arguments: ({0})", string.Join(",", args));
+
+var hostBuilder = Host.CreateDefaultBuilder(args)
+    .ConfigureLogging((hostContext, logBuilder) =>
+    {
+        // ignore Microsoft Debug and Informational logging for this demo
+        logBuilder
+            .SetMinimumLevel(LogLevel.Debug)
+            .AddFilter("Microsoft", LogLevel.Warning);
+
+    })   
+    .ConfigureServices((hostContext, services) =>
+    {
+        // log the configuration after all chained configurations are applied
+        hostContext.LogConfiguration();
+    });
+
+Console.WriteLine("Press Ctrl-C to stop the host and exit.");
+
+var builtHost = hostBuilder.Build();
+
+
+builtHost.Run();
+
+
